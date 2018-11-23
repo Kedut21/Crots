1f4b312b8d92bccff0312cd15085c65713242f90
Kedut21 <41323725+Kedut21@users.noreply.github.com>
2018-11-23 18:57:00 +0700

    Create 18+.sh

---
+++ 18+.sh
@@ -0,0 +1,64 @@
+#!/system/xbin/bash
+clear
+python2 Ikeh.py
+echo "_____________________________________________________________"
+echo •  "Tools    : KHESA TAMVAN/KEDUT21 $white         " |lolcat
+echo •  "Creadby  : Fox Cyber Team $white   " |lolcat
+echo • "Hubungi: 082297538971 $ putih" | lolcat
+echo "_____________________________________________________________"
+echo " "
+echo "inget dosa coeg "
+sleep 1
+echo "gw gk bertanggung jawab"
+sleep 1
+echo "gw gk nanggung dosa lu coeg"
+sleep 1
+echo "Jangan lupa make vpn"
+sleep 1
+echo "ini dia linknya"
+echo "xvideos.com"
+echo "xhamster.com"
+echo "pornhub.com"
+echo "xnxx.com"
+echo "redtube.com"
+echo "youporn.com"
+echo "tube8.com"
+echo "youjizz.com"
+echo "hardsextube.com"
+echo "beeg.com"
+echo "motherless.com"
+echo "drtuber.com"
+echo "nuvid.com"
+echo "pornerbros.com"
+echo "spankwire.com"
+echo "keezmovies.com"
+echo "sunporno.com"
+echo "porn.com"
+echo "4tube.com"
+echo "alphaporno.com"
+echo "xtube.com"
+echo "pornoxo.com"
+echo "yobt.com"
+echo "tnaflix.com"
+echo "pornsharia.com"
+echo "brazzers.com"
+echo "extremetube.com"
+echo "slutload.com"
+echo "fapdu.com"
+echo "empflix.com"
+echo "alotporn.com"
+echo "vid2c.com"
+echo "Shufuni.com"
+echo "cliphunter.com"
+echo "xxxbunker.com"
+echo "madthumbs.com"
+echo "deviantclip.com"
+echo "twilightsex.com"
+echo "pornhost.com"
+echo "fux.com"
+sleep 2
+echo "INGET DOSA KNTL"
+sleep 1
+echo "BTW UMUR GW 14 COEG"
+sleep 1
+echo "kalo mao banyak chat gw"
